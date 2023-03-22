97c56e7c11fa724e60acae1332bcc94816bbeb08
Sergey-bit <71939250+Sergey-bit@users.noreply.github.com>
2023-03-22 21:26:09 +0700

    Update Vector.hpp

--- src/Vector.hpp
+++ src/Vector.hpp
@@ -25,6 +25,12 @@ struct _Trans
 };
 
 template<>
+struct _Trans<unsigned int>
+{
+	using type = int;
+};
+
+template<>
 struct _Trans<float>
 {
 	using type = int;
@@ -53,4 +59,16 @@ inline vec3<typename _Trans<std::_Remove_cvref_t<T>>::type>&& exchangeIF(const v
 		(typename _Trans<std::_Remove_cvref_t<T>>::type)vec.y,
 		(typename _Trans<std::_Remove_cvref_t<T>>::type)vec.z
 		);
+}
+
+template<typename T>
+vec2<T> operator*(const vec2<T>& vec1, const vec2<T>& vec2)
+{
+	return vec2f(vec1.x * vec2.x, vec1.y * vec2.y);
+}
+
+template<typename T>
+vec2f operator/(const vec2<T>& vec1, const vec2<T>& vec2)
+{
+	return vec2f(vec1.x / vec2.x, vec1.y / vec2.y);
 }
